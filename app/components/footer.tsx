5095ed272013736105ec0751216de16240651f00
MAHG <mahg1499@gmail.com>
2026-05-10 01:01:16 -0600

    fix. Corregidos pequeños bugs visuales

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="content-footer">
-      <span>Domus</span>
+      <span>Donum</span>
       <span className="content-footer-separator">·</span>
       <span>© {new Date().getFullYear()}</span>
       <span className="content-footer-separator">·</span>
-      <span><Link href="/about">Sobre Domus</Link></span>
+      <span><Link href="/about">Sobre Donum</Link></span>
       <span className="content-footer-separator">·</span>
-      <span>by <Link href="https://mahg.me" target="_blank">MAHG</Link></span>
+      <span>Dev by <Link href="https://mahg.me" target="_blank">MAHG</Link></span>
     </footer>
   );
 }
